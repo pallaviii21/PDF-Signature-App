666896de9674865cc861625d07ab306d654ca2c6
Pallavi Kumari <pallaviiik11.11@gmail.com>
2025-07-05 08:28:22 +0530

    changes 3

--- server/index.js
+++ server/index.js
@@ -31,10 +31,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.use('/{*splat}', (req, res) => {
-  res.status(404).send('Page not found');
-});
-
 // Middleware
 app.use(express.json());
 
